1f524bfa2a1448c48488bc5e342f95b3f4970b16
Ali2877 <ANGELofICE2877>
2022-09-22 22:23:52 +0500

    SD

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,66 +1,67 @@
 <template>
   <div class="nav">
-    <div
-      class="media-navas px-6 py-3.5 rounded-xl bg-white"
-      style="padding-bottom: 5rem"
-    >
+    <div class="media-navas px-6 py-3.5 rounded-xl bg-white" style="padding-bottom: 5rem">
       <div class="flex justify-center mb-3.5">
-        <nuxt-link to="/"
-          ><img src="@/assets/img/logo.png" alt="" />
+        <nuxt-link to="/"><img src="@/assets/img/logo.png" alt="" />
         </nuxt-link>
       </div>
       <h1 class="text-sm font-mono text-center font-medium mb-3.5">
         {{ $t("logo.description") }}
       </h1>
       <hr />
-      <h2 class="text-center font-bold mt-3">Tizimga kirish</h2>
       <nuxt-link :to="{ name: 'auth-login___' + $i18n.locale }">
-        <div
-          @click="$store.commit('Media_Menu_Close', false)"
-          style="background-color: #3182ce; cursor: pointer"
-          class="flex mt-4 px-2 py-2 rounded items-center"
-        >
-          <nuxt-link to="/"
-            ><div class="p-2 mr-2">
-              <svg
-                width="25"
-                height="25"
-                viewBox="0 0 15 15"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
+        <div @click="$store.commit('Media_Menu_Close', false)" style="background-color:white; cursor: pointer"
+          class="flex mt-4 px-2 py-2 rounded items-center">
+          <nuxt-link to="/">
+            <div class="p-2 mr-2">
+              <svg width="70" height="70" viewBox="0 0 15 15" fill="#3182CE" xmlns="http://www.w3.org/2000/svg">
                 <path
-                  d="M9.74534 1.89199C9.17522 1.27646 8.37893 0.9375 7.50003 0.9375C6.61643 0.9375 5.81751 1.27441 5.25003 1.88613C4.6764 2.50459 4.3969 3.34512 4.46253 4.25273C4.59261 6.04336 5.9552 7.5 7.50003 7.5C9.04485 7.5 10.4051 6.04365 10.5372 4.25332C10.6037 3.35391 10.3225 2.51514 9.74534 1.89199Z"
-                  fill="white"
-                />
+                  d="M9.74534 1.89199C9.17522 1.27646 8.37893 0.9375 7.50003 0.9375C6.61643 0.9375 5.81751 1.27441 5.25003 1.88613C4.6764 2.50459 4.3969 3.34512 4.46253 4.25273C4.59261 6.04336 5.9552 7.5 7.50003 7.5C9.04485 7.5 10.4051 6.04365 10.5372 4.25332C10.6037 3.35391 10.3225 2.51514 9.74534 1.89199Z" />
                 <path
-                  d="M12.6564 14.0625H2.34392C2.20894 14.0643 2.07526 14.0359 1.95262 13.9795C1.82997 13.9231 1.72145 13.84 1.63493 13.7364C1.4445 13.5088 1.36775 13.1979 1.42458 12.8836C1.67185 11.5119 2.44353 10.3597 3.65642 9.55078C4.73396 8.83271 6.0989 8.4375 7.50016 8.4375C8.90143 8.4375 10.2664 8.83301 11.3439 9.55078C12.5568 10.3594 13.3285 11.5116 13.5757 12.8833C13.6326 13.1977 13.5558 13.5085 13.3654 13.7361C13.2789 13.8398 13.1704 13.9229 13.0478 13.9794C12.9251 14.0358 12.7914 14.0642 12.6564 14.0625Z"
-                  fill="white"
-                />
+                  d="M12.6564 14.0625H2.34392C2.20894 14.0643 2.07526 14.0359 1.95262 13.9795C1.82997 13.9231 1.72145 13.84 1.63493 13.7364C1.4445 13.5088 1.36775 13.1979 1.42458 12.8836C1.67185 11.5119 2.44353 10.3597 3.65642 9.55078C4.73396 8.83271 6.0989 8.4375 7.50016 8.4375C8.90143 8.4375 10.2664 8.83301 11.3439 9.55078C12.5568 10.3594 13.3285 11.5116 13.5757 12.8833C13.6326 13.1977 13.5558 13.5085 13.3654 13.7361C13.2789 13.8398 13.1704 13.9229 13.0478 13.9794C12.9251 14.0358 12.7914 14.0642 12.6564 14.0625Z" />
               </svg>
             </div>
           </nuxt-link>
-          <div class="">
-            <h2 class="text-white font-s font-bold mr-3">{{ $t("login.user") }}</h2>
+          <div style="width:100%;"  class="">
+            <div class="">
+              <h2 class="font-s font-bold">{{ $t("login.user") }}</h2>
+            </div>
+            <div class="login__btn mt-2">
+              <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M10.6667 0H1.33333C0.593333 0 0 0.593333 0 1.33333V4H1.33333V1.33333H10.6667V10.6667H1.33333V8H0V10.6667C0 11.0203 0.140476 11.3594 0.390524 11.6095C0.640573 11.8595 0.979711 12 1.33333 12H10.6667C11.0203 12 11.3594 11.8595 11.6095 11.6095C11.8595 11.3594 12 11.0203 12 10.6667V1.33333C12 0.979711 11.8595 0.640573 11.6095 0.390524C11.3594 0.140476 11.0203 0 10.6667 0ZM4.72 8.38667L5.66667 9.33333L9 6L5.66667 2.66667L4.72 3.60667L6.44667 5.33333H0V6.66667H6.44667L4.72 8.38667Z"
+                  fill="white" />
+              </svg>
+              <h2 class="text-center ml-3 font-bold">Tizimga kirish</h2>
+            </div>
           </div>
         </div>
       </nuxt-link>
       <nuxt-link :to="{ name: 'auth-legal___' + $i18n.locale }">
-        <div
-            @click="$store.commit('Media_Menu_Close', false)"
-          style="background-color: #3182ce; cursor: pointer"
-          class="flex mt-2 px-2 py-2 rounded items-center"
-        >
-          <nuxt-link to="/"
-            ><div class="p-2 mr-3">
-              <svg width="25" height="25" viewBox="0 0 106 122" style="fill:white;" xmlns="http://www.w3.org/2000/svg">
-                <path d="M53 61C69.7281 61 83.2857 47.3465 83.2857 30.5C83.2857 13.6535 69.7281 0 53 0C36.2719 0 22.7143 13.6535 22.7143 30.5C22.7143 47.3465 36.2719 61 53 61ZM75.667 68.768L64.3571 114.375L56.7857 81.9688L64.3571 68.625H41.6429L49.2143 81.9688L41.6429 114.375L30.333 68.768C13.4629 69.5781 0 83.4699 0 100.65V110.562C0 116.877 5.08705 122 11.3571 122H94.6429C100.913 122 106 116.877 106 110.562V100.65C106 83.4699 92.537 69.5781 75.667 68.768Z"/>
-                </svg>
-                
+        <div @click="$store.commit('Media_Menu_Close', false)" style="background-color:white; cursor: pointer"
+          class="flex mt-2 px-2 py-2 rounded items-center">
+          <nuxt-link to="/">
+            <div class="p-2 mr-3">
+              <svg width="70" height="70" viewBox="0 0 106 122" style="fill:#3182CE;"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M53 61C69.7281 61 83.2857 47.3465 83.2857 30.5C83.2857 13.6535 69.7281 0 53 0C36.2719 0 22.7143 13.6535 22.7143 30.5C22.7143 47.3465 36.2719 61 53 61ZM75.667 68.768L64.3571 114.375L56.7857 81.9688L64.3571 68.625H41.6429L49.2143 81.9688L41.6429 114.375L30.333 68.768C13.4629 69.5781 0 83.4699 0 100.65V110.562C0 116.877 5.08705 122 11.3571 122H94.6429C100.913 122 106 116.877 106 110.562V100.65C106 83.4699 92.537 69.5781 75.667 68.768Z" />
+              </svg>
+
             </div>
           </nuxt-link>
-          <div class="">
-            <h2 class="text-white font-s font-bold mr-2">{{ $t("login.legal") }}</h2>
+          <div style="width:100%;" class="">
+            <div class="">
+              <h2 class="font-s font-bold ">{{ $t("login.legal") }}</h2>
+            </div>
+            <div class="login__btn mt-2">
+              <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M10.6667 0H1.33333C0.593333 0 0 0.593333 0 1.33333V4H1.33333V1.33333H10.6667V10.6667H1.33333V8H0V10.6667C0 11.0203 0.140476 11.3594 0.390524 11.6095C0.640573 11.8595 0.979711 12 1.33333 12H10.6667C11.0203 12 11.3594 11.8595 11.6095 11.6095C11.8595 11.3594 12 11.0203 12 10.6667V1.33333C12 0.979711 11.8595 0.640573 11.6095 0.390524C11.3594 0.140476 11.0203 0 10.6667 0ZM4.72 8.38667L5.66667 9.33333L9 6L5.66667 2.66667L4.72 3.60667L6.44667 5.33333H0V6.66667H6.44667L4.72 8.38667Z"
+                  fill="white" />
+              </svg>
+              <h2 class="text-center ml-3 font-bold">Tizimga kirish</h2>
+            </div>
           </div>
         </div>
       </nuxt-link>
@@ -76,13 +77,27 @@ export default {};
 @media (max-width: 768px) {
   .nav {
     height: 100%;
+
     .rounded-xl {
       border-radius: 0 !important;
     }
   }
+
   .media-navas {
     border-radius: 0 !important;
     height: 100%;
   }
+}
+
+.login__btn {
+  font-size: 12px;
+  width:100%;
+  justify-content: center;
+  display: flex;
+  align-items: center;
+  padding:15px;
+  background: #3182CE;
+  color: white;
+  border-radius: 15px;
 }
 </style>
