54e344c04c686bc867911c8e3a9ab96d79e7abbf
Umidbek Jumaniyozov <ujumaniyozov@ya.ru>
2022-10-04 01:17:34 +0500

    ASD

--- nuxt.config.js
+++ nuxt.config.js
@@ -187,8 +187,8 @@ export default {
 
     // Axios module configuration: https://go.nuxtjs.dev/config-axios
     axios: {
-        baseURL: "http://localhost:5000/api/v1",
-        // baseURL: 'https://app.zerox.uz/api/v1',
+        // baseURL: "http://localhost:5000/api/v1",
+        baseURL: 'https://app.zerox.uz/api/v1',
         headers: {
             'Access-Control-Allow-Origin': '*',
         },
