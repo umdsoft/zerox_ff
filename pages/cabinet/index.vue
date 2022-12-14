bd403eb19d1a53c4c65317cca00cfd3d06c85739
Nasrullo <nasrulloorinboyev1997@gmail.com>
2022-12-14 17:47:45 +0500

    fjewropw

--- pages/cabinet/index.vue
+++ pages/cabinet/index.vue
@@ -131,7 +131,8 @@
 
             <button
               class="bg-blue-400 w-full hover:bg-blue-500 mt-6 block text-center py-2 text-white text-sm rounded"
-            >
+            > 
+            
               Universal shartnoma
             </button>
           </div>
@@ -177,21 +178,13 @@
               <tr class="border border-blue-300">
                 <td class="border border-blue-300 px-4 py-2">Telefon raqami</td>
                 <td class="border border-blue-300 px-4 py-2">
-                  <div class="flex align-center">
+                  <div class="flex align-center justify-between">
                     <span class="mr-4"> {{ $auth.user.phone }}</span>
                     <nuxt-link to="/rephone" class="flex align-center text-t_primary">
-                      <span class="w-4 h-4 mr-2"
-                        ><svg
-                          class="w-full"
-                          xmlns="http://www.w3.org/2000/svg"
-                          viewBox="0 0 512 512"
-                        >
-                          <path
-                            d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"
-                          />
-                        </svg>
+                      <span class="w-4 h-4 mr-2"  >
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. --><path d="M0 64C0 28.7 28.7 0 64 0H224V128c0 17.7 14.3 32 32 32H384V285.7l-86.8 86.8c-10.3 10.3-17.5 23.1-21 37.2l-18.7 74.9c-2.3 9.2-1.8 18.8 1.3 27.5H64c-35.3 0-64-28.7-64-64V64zm384 64H256V0L384 128zM549.8 235.7l14.4 14.4c15.6 15.6 15.6 40.9 0 56.6l-29.4 29.4-71-71 29.4-29.4c15.6-15.6 40.9-15.6 56.6 0zM311.9 417L441.1 287.8l71 71L382.9 487.9c-4.1 4.1-9.2 7-14.9 8.4l-60.1 15c-5.5 1.4-11.2-.2-15.2-4.2s-5.6-9.7-4.2-15.2l15-60.1c1.4-5.6 4.3-10.8 8.4-14.9z"/></svg>
                       </span>
-                      Raqamni o'zgartirish
+                
                     </nuxt-link>
                   </div>
                 </td>
