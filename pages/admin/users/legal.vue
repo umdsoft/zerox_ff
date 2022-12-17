f8a089a14edbdafffc661fd90bf2ab3df9015acd
Nasrullo <nasrulloorinboyev1997@gmail.com>
2022-12-17 12:10:37 +0500

    btn bitti hammrefas

--- pages/admin/users/legal.vue
+++ pages/admin/users/legal.vue
@@ -17,7 +17,7 @@
               fill="white"
             />
           </svg> 
-          <span class="ml-1">Excelga  yuklash</span>
+          <span class="ml-1">Excelga   yuklash</span>
 
           </div>
         </button>
--- pages/admin/users/users.vue
+++ pages/admin/users/users.vue
@@ -9,7 +9,7 @@
               <svg
                 width="30"
                 height="30"
-                viewBox="0 0 20 20"
+                viewBox="0 0 20 20" 
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
               >
