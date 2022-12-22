24481973cad5deba03dcdeaf1ccf864557d32f3f
Nasrullo <nasrulloorinboyev1997@gmail.com>
2022-12-22 17:34:33 +0500

    hammasi tomom boldi h

--- pages/cabinet/auth/phone.vue
+++ pages/cabinet/auth/phone.vue
@@ -15,7 +15,7 @@
                       <input type="text" @input="removeSpace" class="input " placeholder="Telefon raqamni kiriting"    v-mask="'+998 ## ### ## ##'">
                          <button @click="stepGo" class="bg-t_primary hover:bg-blue-700 text-white mt-6 py-4  px-4 rounded w-full">Davom etish</button>
                
-                </div>
+                </div> 
                
             </div>
                 </div>
