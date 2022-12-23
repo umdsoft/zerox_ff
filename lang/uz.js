00c6a8a95337d8c83856940bbdd37b7d04fa4b69
Nasrullo <nasrulloorinboyev1997@gmail.com>
2022-12-23 17:16:32 +0500

    sfvauwerlurdrthrtdfhrtdthi

--- lang/uz.js
+++ lang/uz.js
@@ -202,7 +202,6 @@
       tx:'Foydalanish yo‘riqnomasi',
       txt:'Barcha xizmatlarni saytdan, mobil ilovasidan, yoki shunchaki sutkalik aloqa markaziga qo‘ng‘iroq qilib olish mumkin. Biz aynan shunday konsepsiyani tanladik, chunki u bugungi kunda o‘zini to‘liq oqlay oladi. Mamlakat aholisi yanada mobilroq bo‘lib qoldi, raqamli texnologiyalardan foydalanishni va ko‘pgina xizmatlarni masofadan va dunyoning istalgan nuqtasidan sutka davomida olishni afzal ko‘radi. Shaharda bizning odatiy bo‘limlarimiz yo‘q, biz mamlakat bo‘ylab millionlab qurilmalarda erishimlimiz. Har bir mijozga nisbatan individual yondashuvni saqlab va ma‘lumotlarga ishlov berishda xavfsizlikni kafolatlab, har qanday so‘rovga maksimal operativ tarzda ishlov berish uchun biz ulkan sa‘y-harakatlar qildik. Barcha xizmatlarni saytdan, mobil ilovasidan, yoki shunchaki sutkalik aloqa markaziga qo‘ng‘iroq qilib olish mumkin. Biz aynan shunday konsepsiyani tanladik, chunki u bugungi kunda o‘zini to‘liq oqlay oladi. Mamlakat aholisi yanada mobilroq bo‘lib qoldi, raqamli texnologiyalardan foydalanishni va ko‘pgina xizmatlarni masofadan va dunyoning istalgan nuqtasidan sutka davomida olishni afzal ko‘radi. Shaharda bizning odatiy bo‘limlarimiz yo‘q, biz mamlakat bo‘ylab millionlab qurilmalarda erishimlimiz. Har bir mijozga nisbatan individual yondashuvni saqlab va ma‘lumotlarga ishlov berishda xavfsizlikni kafolatlab, har qanday so‘rovga maksimal operativ tarzda ishlov berish uchun biz ulkan sa‘y-harakatlar qildik. Barcha xizmatlarni saytdan, mobil ilovasidan, yoki shunchaki sutkalik aloqa markaziga qo‘ng‘iroq qilib olish mumkin. Biz aynan shunday konsepsiyani tanladik, chunki u bugungi kunda o‘zini to‘liq oqlay oladi. Mamlakat aholisi yanada mobilroq bo‘lib qoldi, raqamli texnologiyalardan foydalanishni va ko‘pgina xizmatlarni masofadan va dunyoning istalgan nuqtasidan sutka davomida olishni afzal ko‘radi. Shaharda bizning odatiy bo‘limlarimiz yo‘q, biz mamlakat bo‘ylab millionlab qurilmalarda erishimlimiz. Har bir mijozga nisbatan individual yondashuvni saqlab va ma‘lumotlarga ishlov berishda xavfsizlikni kafolatlab, har qanday so‘rovga maksimal operativ tarzda ishlov berish uchun biz ulkan sa‘y-harakatlar qildik. Barcha xizmatlarni saytdan, mobil ilovasidan, yoki shunchaki sutkalik aloqa markaziga qo‘ng‘iroq qilib olish mumkin. Biz aynan shunday konsepsiyani tanladik, chunki u bugungi kunda o‘zini to‘liq oqlay oladi. Mamlakat aholisi yanada mobilroq bo‘lib qoldi, raqamli texnologiyalardan foydalanishni va ko‘pgina xizmatlarni masofadan va dunyoning istalgan nuqtasidan sutka davomida olishni afzal ko‘radi. Shaharda bizning odatiy bo‘limlarimiz yo‘q, biz mamlakat bo‘ylab millionlab qurilmalarda erishimlimiz. Har bir mijozga nisbatan individual yondashuvni saqlab va ma‘lumotlarga ishlov berishda xavfsizlikni kafolatlab, har qanday so‘rovga maksimal operativ tarzda ishlov berish uchun biz ulkan sa‘y-harakatlar qildik. ',
 
-
    },
    mobil:{
       bck:'Oqaga',
@@ -273,7 +272,6 @@
       a30:'     Tasdiq kodini kiriting',
       a31:' Kodlar mos emas',
       a32:' Tasdiqlash' ,
-      
       a1:'Mobil hisobga o‘tkazma',
       a34:'Jo‘natuvchi',
       a35:'Qabul qiluvchi',
