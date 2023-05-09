888ff567ce2acbdc3c9f08bcb3b2456ad9db5867
Umidbek Jumaniyozov <63510583+umdsoft@users.noreply.github.com>
2023-05-09 14:59:30 +0500

    Update index.vue

--- pages/give-money/index.vue
+++ pages/give-money/index.vue
@@ -254,6 +254,9 @@ export default {
     console.log("user", this.user);
   },
   mounted() {
+    if(this.$auth.user.is_active == 1 && this.$auth.user.is_contract == 0){
+        this.$router.push("/universal_contract");
+      }
     setTimeout(() => {
       function keydownInput(e) {}
       let input = document.querySelector(".mx-input");
