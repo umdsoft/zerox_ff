33af1399c94c444f352fc4b41950488cccc662f7
abc <abc@Umidbek-Jumaniyozov.local>
2024-04-17 23:32:12 +0500

    ADS

--- pages/cabinet/index.vue
+++ pages/cabinet/index.vue
@@ -75,69 +75,10 @@
             <p class="text-sm text-center">{{ $t('transfer.id') }}</p>
             <p class="text-blue-400 text-sm">{{ user.uid }}</p>
           </div>
-          <!-- <div class="flex status">
-
-            <span class=" px-4 pt-3">Status</span>
-            <span class=" pl-10 pt-3">
-              <div class="flex">
-                {{ user.rating }}
-                <span v-if="user.rating_type == 1">
-                  <svg width="14" height="10" viewBox="0 0 14 10" fill="none" xmlns="http://www.w3.org/2000/svg"
-                    style="margin: 6px 0px 0px 6px">
-                    <path d="M0 5L0 9.5L7 4.5L14 9.5V5L7 0L0 5Z" fill="#049D26" />
-                  </svg>
-                </span>
-                <span v-if="user.rating_type == 2">
-                  <svg width="14" height="17" viewBox="0 0 14 17" fill="none" style="margin: 6px 0px 0px 6px"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path d="M0 5L0 9.5L7 4.5L14 9.5V5L7 0L0 5Z" fill="#049D26" />
-                    <path d="M0 12L0 16.5L7 11.5L14 16.5V12L7 7L0 12Z" fill="#049D26" />
-                  </svg>
-                </span>
-                <span v-if="user.rating_type == 3">
-                  <svg width="14" height="10" viewBox="0 0 14 10" fill="none" style="margin: 6px 0px 0px 6px"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path d="M14 4.5V0L7 5L0 0V4.5L7 9.5L14 4.5Z" fill="#FF0000" />
-                  </svg>
-                </span>
-                <span v-if="user.rating_type == 4">
-                  <svg width="14" height="17" viewBox="0 0 14 17" fill="none" style="margin: 6px 0px 0px 6px"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path d="M14 11.5V7L7 12L0 7V11.5L7 16.5L14 11.5Z" fill="#FF0000" />
-                    <path d="M14 4.5V0L7 5L0 0V4.5L7 9.5L14 4.5Z" fill="#FF0000" />
-                  </svg>
-                </span>
-              </div>
-            </span>
-          </div> -->
-
+       
           <div class="px-4">
-            <!-- <nuxt-link v-if="user.type == 2" to="/repassword"
-              class="bg-blue-400 w-full hover:bg-blue-500 mt-6 block text-center  text-white text-sm rounded bt b">
-
-              <svg width="40" height="36" viewBox="0 0 40 36" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path fill-rule="evenodd" clip-rule="evenodd"
-                  d="M20.7325 11.7985C20.2638 11.3297 20.0005 10.6939 20.0005 10.031C20.0005 9.36806 20.2638 8.73227 20.7325 8.26344L28.2327 0.763241C28.4633 0.524459 28.7392 0.333999 29.0442 0.202973C29.3492 0.0719466 29.6773 0.00297901 30.0092 9.43943e-05C30.3412 -0.00279022 30.6704 0.0604658 30.9776 0.186171C31.2849 0.311877 31.564 0.497515 31.7988 0.732253C32.0335 0.966991 32.2191 1.24613 32.3448 1.55338C32.4705 1.86063 32.5338 2.18983 32.5309 2.52179C32.528 2.85375 32.4591 3.18181 32.328 3.48682C32.197 3.79184 32.0065 4.06771 31.7678 4.29834L24.2676 11.7985C23.7987 12.2672 23.1629 12.5305 22.5 12.5305C21.8371 12.5305 21.2013 12.2672 20.7325 11.7985Z"
-                  fill="white" />
-                <path fill-rule="evenodd" clip-rule="evenodd"
-                  d="M39.2679 11.7986C38.799 12.2673 38.1632 12.5306 37.5003 12.5306C36.8374 12.5306 36.2016 12.2673 35.7328 11.7986L28.2326 4.29842C27.7772 3.8269 27.5252 3.19538 27.5309 2.53987C27.5366 1.88436 27.7995 1.25731 28.263 0.793776C28.7265 0.330244 29.3536 0.0673141 30.0091 0.0616179C30.6646 0.0559217 31.2961 0.307914 31.7677 0.763322L39.2679 8.26352C39.7365 8.73235 39.9998 9.36814 39.9998 10.0311C39.9998 10.694 39.7365 11.3298 39.2679 11.7986Z"
-                  fill="white" />
-                <path fill-rule="evenodd" clip-rule="evenodd"
-                  d="M29.9999 5.03223C30.663 5.03223 31.2989 5.29563 31.7677 5.76448C32.2366 6.23333 32.5 6.86923 32.5 7.53229V27.5328C32.5 28.1959 32.2366 28.8318 31.7677 29.3006C31.2989 29.7695 30.663 30.0329 29.9999 30.0329C29.3369 30.0329 28.7009 29.7695 28.2321 29.3006C27.7632 28.8318 27.4998 28.1959 27.4998 27.5328V7.53229C27.4998 6.86923 27.7632 6.23333 28.2321 5.76448C28.7009 5.29563 29.3369 5.03223 29.9999 5.03223ZM19.2671 23.2652C19.7358 23.734 19.9991 24.3698 19.9991 25.0328C19.9991 25.6957 19.7358 26.3315 19.2671 26.8003L11.7669 34.3005C11.2954 34.7559 10.6639 35.0079 10.0084 35.0022C9.35286 34.9965 8.72582 34.7336 8.26228 34.2701C7.79875 33.8065 7.53582 33.1795 7.53012 32.524C7.52443 31.8685 7.77642 31.2369 8.23183 30.7654L15.732 23.2652C16.2009 22.7965 16.8366 22.5332 17.4996 22.5332C18.1625 22.5332 18.7983 22.7965 19.2671 23.2652Z"
-                  fill="white" />
-                <path fill-rule="evenodd" clip-rule="evenodd"
-                  d="M0.731985 23.2642C1.20082 22.7955 1.8366 22.5322 2.49953 22.5322C3.16246 22.5322 3.79825 22.7955 4.26708 23.2642L11.7673 30.7644C12.0061 30.995 12.1965 31.2709 12.3275 31.5759C12.4586 31.8809 12.5275 32.209 12.5304 32.541C12.5333 32.8729 12.4701 33.2021 12.3444 33.5094C12.2186 33.8166 12.033 34.0958 11.7983 34.3305C11.5635 34.5652 11.2844 34.7509 10.9771 34.8766C10.6699 35.0023 10.3407 35.0655 10.0087 35.0627C9.67677 35.0598 9.34871 34.9908 9.0437 34.8598C8.73868 34.7287 8.46281 34.5383 8.23219 34.2995L0.731985 26.7993C0.263295 26.3305 0 25.6947 0 25.0318C0 24.3688 0.263295 23.733 0.731985 23.2642Z"
-                  fill="white" />
-                <path fill-rule="evenodd" clip-rule="evenodd"
-                  d="M9.99958 30.0329C9.33652 30.0329 8.70062 29.7695 8.23176 29.3006C7.76291 28.8318 7.49951 28.1959 7.49951 27.5328V7.53229C7.49951 6.86923 7.76291 6.23333 8.23176 5.76448C8.70062 5.29563 9.33652 5.03223 9.99958 5.03223C10.6626 5.03223 11.2985 5.29563 11.7674 5.76448C12.2362 6.23333 12.4996 6.86923 12.4996 7.53229V27.5328C12.4996 28.1959 12.2362 28.8318 11.7674 29.3006C11.2985 29.7695 10.6626 30.0329 9.99958 30.0329Z"
-                  fill="white" />
-              </svg>
-
-              <span>
-                Status holati
-              </span>
-            </nuxt-link> -->
-            <nuxt-link v-if="user.type == 2" to="/repassword"
+           
+            <nuxt-link v-if="user.type == 2" :to="{name:'repassword___'+ $i18n.locale }"
               class="bg-blue-400 w-full hover:bg-blue-500 mt-3 block text-center py-2 text-white text-sm rounded bt b">
               <svg xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 448 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
@@ -203,7 +144,7 @@
                 <td class="border border-blue-300 px-4 py-2">
                   <div class="flex align-center justify-between">
                     <span class="mr-4"> {{ user.phone }}</span>
-                    <nuxt-link to="/rephone" class="flex align-center text-t_primary">
+                    <nuxt-link :to="{name:'rephone___'+ $i18n.locale }" class="flex align-center text-t_primary">
                       <span class="pan w-4 h-4 mr-2">
                         <svg class="fill-blue-300" xmlns="http://www.w3.org/2000/svg"
                           viewBox="0 0 576 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
@@ -261,7 +202,7 @@
                 <td class="border border-blue-300 px-4 py-2">
                   <div class="flex align-center justify-between">
                     <span class="mr-4"> {{ user.phone }}</span>
-                    <nuxt-link to="/rephone" class="flex align-center text-t_primary">
+                    <nuxt-link :to="{name:'rephone___'+ $i18n.locale }" class="flex align-center text-t_primary">
                       <span class="pan w-4 h-4 mr-2">
                         <svg class="fill-blue-300" xmlns="http://www.w3.org/2000/svg"
                           viewBox="0 0 576 512"><!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
